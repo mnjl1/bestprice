037bed70a8bb92e3639606810277cc5db2dd9339
Dmytro Manzhula <d.manzhula@gmail.com>
2018-10-30 14:58:29 +0200

    19-create-order-report

--- src/main/java/com/mmplus/bestprice/controller/HotPriceScheduleController.java
+++ src/main/java/com/mmplus/bestprice/controller/HotPriceScheduleController.java
@@ -1,9 +1,16 @@
 package com.mmplus.bestprice.controller;
 
+import com.mmplus.bestprice.entity.HotPriceOrder;
 import com.mmplus.bestprice.entity.HotPriceSchedule;
 import com.mmplus.bestprice.service.HotPriceScheduleService;
+
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,5 +41,19 @@ public class HotPriceScheduleController {
     public String getBestPriceEventsList(Model model){
         model.addAttribute("hotPriceEvents", hotPriceScheduleService.findAll());
         return "hot-price-schedule-list";
+    }
+    
+    @RequestMapping("/hot-price-schedule-list-for-process")
+    public String getHotPriceEventsListForProcess(Model model){
+        model.addAttribute("hotPriceEvents", hotPriceScheduleService.findAll());
+        return "hot-price-schedule-list-for-process";
+    }
+    
+    @RequestMapping("/hot-price-schedule-list-per-date/{id}")
+    public String getOdersListPerDate(@PathVariable (value="id") Long id, Model model) {
+    			Set<HotPriceOrder> ordersPerDate = hotPriceScheduleService.findById(id).getOrders();
+    			model.addAttribute("ordersPerDate", ordersPerDate);
+ 
+    	return "hot-price-schedule-list-per-date";
     }
 }
